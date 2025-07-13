6eef5aacbfd173f12aef1c69a96982b98b618a25
SubrataP <subratap.eid@gmail.com>
2025-07-13 20:09:54 +0530

    chore: cleanup release branch with only dist and public files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-//src/index.js
-import { init } from './js_modules/core.js';
-init(); // auto-run on initiate
-// Export each saperate modules
-export * from './js_modules/utils.js';
-export * from './js_modules/copyToClipboard.js';
